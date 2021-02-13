d92f4fb9e5d5b94b5c964069fc55e391db0585f7
aurelia Segarra <av.segarra@gmail.com>
2021-02-13 15:15:48 +0100

    initial commit and fix bugs

---
+++ src/components/App/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Menu from 'src/components/Menu';
+import Home from 'src/components/Home';
+import Recipe from 'src/components/Recipe';
+import Error from 'src/components/Error';
+
+import recipesTest from 'src/data';
+
+import Loading from './Loading';
+
+import './style.scss';
+
+function App(props) {
+  const recipes = {
+    list: recipesTest,
+  };
+  // eslint-disable-next-line react/destructuring-assignment
+  if (props.loading) {
+    return <Loading />;
+  }
+  return (
+    <div className="app">
+      <Menu recipes={recipes.list} />
+      <Home recipes={recipes.list} />
+      <Recipe recipe={recipesTest[0]} />
+      <Error />
+    </div>
+  );
+}
+
+App.propTypes = {
+  loading: PropTypes.bool,
+};
+
+App.defaultProps = {
+  loading: false,
+};
+
+export default App;
