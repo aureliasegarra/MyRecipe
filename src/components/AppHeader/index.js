c175b87d6ba65041f4bb17b145dcea720a2e3ad2
aurelia Segarra <av.segarra@gmail.com>
2021-02-17 09:21:01 +0100

    add loginForm

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginForm from 'src/components/LoginForm';
 
 import './style.scss';
 
 const AppHeader = () => (
   <header className="header">
     <h1 className="header__title">myRecipe</h1>
+    <LoginForm />
   </header>
 );
 
