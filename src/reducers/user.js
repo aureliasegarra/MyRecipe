48be86f0466a4c1db5d2b7f650cf9e1e8072732f
aurelia Segarra <av.segarra@gmail.com>
2021-02-17 10:04:26 +0100

    modification case in reducers/user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,3 +1,5 @@
+import { CHANGE_VALUE } from 'src/actions/user';
+
 const initialState = {
   email: 'vincent@gmail.com',
   password: 'aze',
@@ -8,6 +10,12 @@ const initialState = {
 
 const user = (state = initialState, action = {}) => {
   switch (action.type) {
+    case CHANGE_VALUE: {
+      return {
+        ...state,
+        [action.name]: action.value, // dynamization of the value according to the field with []
+      };
+    }
     default:
       return state;
   }
